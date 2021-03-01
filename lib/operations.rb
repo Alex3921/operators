00c31c4d0db6d5d3e9eb7e40bdc7bed2b2f569c0
alexandru <boncut.alex10@gmail.com>
2021-03-01 13:02:50 -0600

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,22 @@
 def unsafe?(speed)
-
+  if speed < 40 || speed > 60
+    true
+  else 
+    false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+  (speed < 40 || speed > 60) ? true : false	
 end
 	
 
+unsafe?(50)
+unsafe?(35)
+unsafe?(65)
 
+not_safe?(50)
+not_safe?(35)
+not_safe?(65)
